219fb61adda7b23351c0e47aff0cadbf983fa728
Bob <bobthinkpad@thinkpad.com>
2026-06-24 16:32:28 +0200

    ui: add BTC MAP to default sidebar after Events and bump order version

--- src/App.tsx
+++ src/App.tsx
@@ -143,8 +143,9 @@ const hardcodedConfig: AppConfig = {
     "pets",
     "wallet",
     "events",
+    "btcmap",
   ],
-  sidebarOrderVersion: 3,
+  sidebarOrderVersion: 4,
   themeDefaultVersion: 1,
   nip85StatsPubkey:
     "5f68e85ee174102ca8978eef302129f081f03456c884185d5ec1c1224ab633ea",
