d967f5ee197348f79c8508f4bfd97c48327e94c1
2140wtf <hello@2140.wtf>
2026-08-03 23:05:23 +0100

    Restore BAO API authentication behavior

--- src/lib/baoWalletApi.ts
+++ src/lib/baoWalletApi.ts
@@ -56,7 +56,6 @@ export async function fetchBaoWalletBalances(signer: BaoApiSigner): Promise<BaoW
   const res = await fetch(url, {
     method: 'GET',
     headers: { Authorization: await baoNip98Header(signer, url, 'GET') },
-    signal: AbortSignal.timeout(15_000),
   });
   const json = (await res.json().catch(() => ({}))) as BalanceEnvelope;
   if (!res.ok) {
